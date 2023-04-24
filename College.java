c170b8bb0f7cb1abdd3178162597aced18f5fbf5
unknown <darshan.hm.xworkz@gmail.com>
2023-04-24 11:41:05 +0530

    forLoop

---
+++ College.java
@@ -0,0 +1,18 @@
+class College{
+
+public static void main(String[] college){
+
+String deptNames[]={"bilogy","chemistry","physics","computer Sciense","econmics","business Studies","histry","geogeropy","mathematics","statstics"};
+
+System.out.println("deptNames are"  +      deptNames.length);
+
+for(int i=0;i<deptNames.length; i++){
+System.out.println(deptNames[i]);
+
+}
+
+
+}
+
+
+}
