a6fb951250c8741658c495820ff06decab1d4055
unknown <darshan.hm.xworkz@gmail.com>
2023-04-27 10:12:31 +0530

    method overloding 1

---
+++ AdditionTester.java
@@ -0,0 +1,15 @@
+class AdditionTester{
+
+public static void main(String[] addition){
+	
+	int addNum=Addition.add(23,34);
+	int addwithAnother=Addition.add(34,45,234);
+	
+	System.out.println("two number is " +addNum);
+	System.out.println("three number is"  +addwithAnother);
+	
+}
+
+
+
+}
